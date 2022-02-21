5175d5db4c4ed76e15980fe10768600a718a793a
Donatella7 <campaniellodonatella@gmail.com>
2022-02-21 15:32:40 +0100

    string = concatenazione di stringhe

--- js/myscript.js
+++ js/myscript.js
@@ -3,9 +3,13 @@ const userName = prompt("Inserisci il tuo nome");
 document.getElementById('nameUser').innerHTML = userName;
 
 /* surname */
-const surnameUser = prompt("Inserisci il tuo nome");
+const surnameUser = prompt("Inserisci il tuo cognome");
 document.getElementById('surname').innerHTML =  surnameUser;
 
 /* colour */
-const favouriteColor = prompt("Inserisci il tuo nome");
+const favouriteColor = prompt("Inserisci il tuo colore preferito");
 document.getElementById('color').innerHTML =  favouriteColor;
+
+/* concatenazione stringhe */
+const userPassword = (userName + surnameUser + favouriteColor);
+document.getElementById('password').innerHTML =  userPassword;

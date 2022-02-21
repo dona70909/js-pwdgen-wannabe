9d436c0105968b1890063a88e48120aea331d111
Donatella7 <campaniellodonatella@gmail.com>
2022-02-21 15:26:41 +0100

    colour

--- js/myscript.js
+++ js/myscript.js
@@ -5,3 +5,7 @@ document.getElementById('nameUser').innerHTML = userName;
 /* surname */
 const surnameUser = prompt("Inserisci il tuo nome");
 document.getElementById('surname').innerHTML =  surnameUser;
+
+/* colour */
+const favouriteColor = prompt("Inserisci il tuo nome");
+document.getElementById('color').innerHTML =  favouriteColor;

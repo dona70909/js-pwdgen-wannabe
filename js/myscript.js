7944582492cbe0ab043a751d45e505e1876e8be6
Donatella7 <campaniellodonatella@gmail.com>
2022-02-21 15:23:48 +0100

    surname

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,7 @@
 /* User name  */
 const userName = prompt("Inserisci il tuo nome");
 document.getElementById('nameUser').innerHTML = userName;
+
+/* surname */
+const surname = prompt("Inserisci il tuo nome");
+document.getElementById('surnameUser').innerHTML =  surname;
